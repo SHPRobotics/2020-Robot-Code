ebb712a13d7f0529f99c5c41bde7e91dcaed7f86
Seton Hall Prep Robotics <setonhallrobotics@gmail.com>
2021-03-13 13:33:24 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -12,16 +12,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.*;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.TimedRobot;
 
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+//import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.XboxController;
 
 
@@ -51,42 +58,128 @@ public class Robot extends TimedRobot {
   CANSparkMax vIntake = new CANSparkMax(9, MotorType.kBrushed);
   CANSparkMax hopper = new CANSparkMax(7, MotorType.kBrushed);
 
+  Servo servo = new Servo(0);
+  ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+
   Joystick leftJoy = new Joystick(0);
   Joystick rightJoy = new Joystick(1);
   XboxController xbox = new XboxController(2);
 
-  DifferentialDrive diffDrive = new DifferentialDrive(leadMotorLeft, leadMotorRight);
+  UsbCamera driveCamera;
 
-  Limelight vision = new Limelight(rightJoy, diffDrive);
+  //DifferentialDrive diffDrive = new DifferentialDrive(leadMotorLeft, leadMotorRight);
+  //NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  //Limelight vision = new Limelight(rightJoy, diffDrive);
   /**
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
    */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+  
+  public void forward (double inches)
+  {
+    double leadLeftOutput1 = leadLeftEncoder.getPosition();
+    double weightAdjust = 1.02;
+    double speed = 0.2;
+    double distance = 0.5 * inches;
 
-    leadMotorRight.restoreFactoryDefaults();
-    followMotorRight.restoreFactoryDefaults();
-    leadMotorLeft.restoreFactoryDefaults();
-    followMotorLeft.restoreFactoryDefaults();
-    shooter1.restoreFactoryDefaults();
-    shooter2.restoreFactoryDefaults();
-    hIntake.restoreFactoryDefaults();
-    vIntake.restoreFactoryDefaults();
-    hopper.restoreFactoryDefaults();
+    while(leadLeftEncoder.getPosition() < distance + leadLeftOutput1) 
+    {
+      leadMotorLeft.set(speed);
+      leadMotorRight.set(-speed*weightAdjust);
+    }
+    leadMotorLeft.set(0.0);
+    leadMotorRight.set(0.0);
+  }
 
-    followMotorRight.follow(leadMotorRight);
-    followMotorLeft.follow(leadMotorLeft);
+  public void backward (double inches)
+  {
+    double leadLeftOutput1 = leadLeftEncoder.getPosition();
+    double weightAdjust = 1.02;
+    double speed = 0.2;
+    double distance = 0.5 * inches;
+
+    while(leadLeftEncoder.getPosition() > -distance + leadLeftOutput1) 
+    {
+      leadMotorLeft.set(-speed);
+      leadMotorRight.set(speed*weightAdjust);
+    }
+    leadMotorLeft.set(0.0);
+    leadMotorRight.set(0.0);
+  }
+
+  public void turnRight (double degrees) {
+    double distance = degrees * 10.69/90;
+    double weightAdjust = 1.0115;
+    double speed = 0.2;
+
+    double leadLeftOutput1 = leadLeftEncoder.getPosition();
+    
+    while(leadLeftEncoder.getPosition() < distance + leadLeftOutput1) 
+    {
+      leadMotorLeft.set(speed);
+      leadMotorRight.set(speed*weightAdjust);
+    }
+    leadMotorLeft.set(0.0);
+    leadMotorRight.set(0.0);
+  }
+
+  public void turnLeft (double degrees) {
+    double distance = degrees * 10.75/90;
+    double weightAdjust = 1.02;
+    double speed = 0.2;
+    double leadLeftOutput1 = leadLeftEncoder.getPosition();
+    
+    while(leadLeftEncoder.getPosition() > -distance + leadLeftOutput1) 
+    {
+      leadMotorLeft.set(-speed);
+      leadMotorRight.set(-speed*weightAdjust);
+    }
+    leadMotorLeft.set(0.0);
+    leadMotorRight.set(0.0);
+  }
+
+  public void brake () {
+
+    double leftVelocity = leadLeftEncoder.getVelocity()/2000;
+    double rightVelocity = leadRightEncoder.getVelocity()/2000;
+
+    SmartDashboard.putNumber("leftVelocity", leftVelocity);
+    SmartDashboard.putNumber("rightVelocity", rightVelocity);
+
+    double watch = System.currentTimeMillis();
+    double time = 150; //ms
+
+    if(rightVelocity > 0) {
+      while (leadRightEncoder.getVelocity()/2000 > 0.1 && System.currentTimeMillis() < watch + time) {
+        leadMotorLeft.set(-leftVelocity);
+        leadMotorRight.set(-rightVelocity);
+      }
+    }
+    else {
+      while (leadRightEncoder.getVelocity()/2000 < 0.1 && System.currentTimeMillis() < watch + time) {
+        leadMotorLeft.set(-leftVelocity);
+        leadMotorRight.set(-rightVelocity);
+      }
+    }
+
+    leadMotorLeft.set(0.0);
+    leadMotorRight.set(0.0);
+  }
+
+  public void wait (int milliseconds) {
+    double watch = System.currentTimeMillis();
+
+    while (System.currentTimeMillis() - watch <= milliseconds) {
+      leadMotorLeft.set(0.0);
+      leadMotorRight.set(0.0);
+    }
   }
 
   public void GRRAA ()
   {
     double watch = System.currentTimeMillis();
     
-    while (System.currentTimeMillis() - watch <= 200)
+    while (System.currentTimeMillis() - watch <= 2000)
     {
       if (xbox.getBackButton())
       {
@@ -101,28 +194,28 @@ public class Robot extends TimedRobot {
         vIntake.set(0);
       }
 
-      if (System.currentTimeMillis() - watch <= 100)
-      {
+      if(xbox.getRawButtonPressed(4)) {
         hopper.set(-0.3);
       }
-      else if (100 < System.currentTimeMillis() - watch && System.currentTimeMillis() - watch <= 200)
-      {
+      else if (xbox.getRawButtonPressed(5)) {
+        shooter1.set(0);
+        shooter2.set(0);
+        break;
+      }
+      else {
+
         hopper.set(0);
+
+        if (System.currentTimeMillis() - watch <= 150)
+        {
+          hopper.set(-0.3);
+        }
+        else if (150 < System.currentTimeMillis() - watch && System.currentTimeMillis() - watch <= 2000)
+        {
+          hopper.set(0);
+        }
       }
     }
-  }
-
-  public void forward (int distance)
-  {
-    double leadLeftOutput1 = leadLeftEncoder.getPosition();
-    
-    while(leadLeftEncoder.getPosition() < distance + leadLeftOutput1) 
-    {
-      leadMotorLeft.set(0.2);
-      leadMotorRight.set(-0.2);
-    }
-    leadMotorLeft.set(0.0);
-    leadMotorRight.set(0.0);
   }
 
   public void DOTDOOTDOOT ()
@@ -134,8 +227,8 @@ public class Robot extends TimedRobot {
 
       if (System.currentTimeMillis() - watch <= 2000)
       {
-        shooter1.set(-0.6);
-        shooter2.set(-0.6);
+        shooter1.set(-1);
+        shooter2.set(-1);
       }
       else if (2000 <= System.currentTimeMillis() - watch && System.currentTimeMillis() - watch <= 8000)
       {
@@ -153,7 +246,7 @@ public class Robot extends TimedRobot {
   {
     double watch = System.currentTimeMillis();
     
-    while (System.currentTimeMillis() - watch <= 7000) 
+    while (System.currentTimeMillis() - watch <= 15000) 
     {
       if (xbox.getBackButton())
       {
@@ -168,17 +261,68 @@ public class Robot extends TimedRobot {
         vIntake.set(0);
       }
 
-      if (System.currentTimeMillis() - watch <= 2000)
+      if (System.currentTimeMillis() - watch <= 3000)
       {
-        shooter1.set(-0.8);
-        shooter2.set(-0.8);
+        shooter1.set(-1);
+        shooter2.set(-1);
         
       }
-      else if (2000 <= System.currentTimeMillis() - watch && System.currentTimeMillis() - watch <= 7000)
+      else if (3000 <= System.currentTimeMillis() - watch)
       {
         GRRAA();
       }
+
+      if(xbox.getRawButtonPressed(5)) {
+        shooter1.set(0);
+        shooter2.set(0);
+        break;
+      }
     }
+  }
+
+  public String chooseRoute (double tx, double distance) {
+    String routeChoice = "";
+
+    if(tx > -5 && tx < 5 && distance < 85 && distance > 65) {
+      routeChoice = "Route A - straight ahead";
+    }
+    else if(tx < -5 && tx > -15 && distance < 200 & distance > 160) {
+      routeChoice = "Route B - turn a little bit";
+    }
+    else {
+      routeChoice = "no route detected";
+    }
+
+    return routeChoice;
+  }
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+      
+    driveCamera = CameraServer.getInstance().startAutomaticCapture();
+    driveCamera.setResolution(640, 480);
+
+    leadMotorRight.restoreFactoryDefaults();
+    followMotorRight.restoreFactoryDefaults();
+    leadMotorLeft.restoreFactoryDefaults();
+    followMotorLeft.restoreFactoryDefaults();
+    shooter1.restoreFactoryDefaults();
+    shooter2.restoreFactoryDefaults();
+    hIntake.restoreFactoryDefaults();
+    vIntake.restoreFactoryDefaults();
+    hopper.restoreFactoryDefaults();
+
+    followMotorRight.follow(leadMotorRight);
+    followMotorLeft.follow(leadMotorLeft);
+
+    servo.setAngle(300);
+
+    gyro.calibrate();
+    SmartDashboard.putNumber("Done Calibrating:", 1);
+    gyro.reset();
+    //table.getEntry("camMode").setNumber(0);
   }
 
   /**
@@ -192,7 +336,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-
+    //System.out.print(System.currentTimeMillis());
   }
 
   /**
@@ -213,8 +357,31 @@ public class Robot extends TimedRobot {
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
 
-    DOTDOOTDOOT();
-    forward(10);
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    
+    double tx = table.getEntry("tx").getDouble(0.0);
+    double ty = table.getEntry("ty").getDouble(0.0);
+    double ta = table.getEntry("ta").getDouble(0.0);
+    //double ts = table.getEntry("ts").getDouble(0.0);
+    table.getEntry("camMode").setNumber(0);
+
+    //System.out.print("tx: " + tx +"\r");
+    //System.out.print("ty: " + ty +"\r");
+    SmartDashboard.putNumber("tx", tx);
+    SmartDashboard.putNumber("ty", ty);
+    SmartDashboard.putNumber("ta", ta);
+    //SmartDashboard.putNumber("ts", ts);
+
+    double distanceInInches = 53.294 * Math.pow(ta, -0.459) + 3;
+
+    SmartDashboard.putNumber("distanceInInches", distanceInInches);
+
+    
+    String routeChoice = chooseRoute(tx, distanceInInches);
+
+    SmartDashboard.putString("Route Choice", routeChoice);
+    
+    
   }
 
   /**
@@ -222,6 +389,33 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
+    
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    
+    double tx = table.getEntry("tx").getDouble(0.0);
+    double ty = table.getEntry("ty").getDouble(0.0);
+    double ta = table.getEntry("ta").getDouble(0.0);
+    //double ts = table.getEntry("ts").getDouble(0.0);
+    table.getEntry("camMode").setNumber(0);
+
+    //System.out.print("tx: " + tx +"\r");
+    //System.out.print("ty: " + ty +"\r");
+    SmartDashboard.putNumber("tx", tx);
+    SmartDashboard.putNumber("ty", ty);
+    SmartDashboard.putNumber("ta", ta);
+    SmartDashboard.putNumber("Rotation", gyro.getAngle());
+    //SmartDashboard.putNumber("ts", ts);
+
+    double distanceInInches = 53.294 * Math.pow(ta, -0.459) + 3;
+
+    SmartDashboard.putNumber("distanceInInches", distanceInInches);
+
+    
+    String routeChoice = chooseRoute(tx, distanceInInches);
+
+    SmartDashboard.putString("Route Choice", routeChoice);
+
+
     switch (m_autoSelected) {
     case kCustomAuto:
       // Put custom auto code here   
@@ -245,7 +439,29 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     
-    //Drive
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    double tx = table.getEntry("tx").getDouble(0.0);
+    double ty = table.getEntry("ty").getDouble(0.0);
+    double ta = table.getEntry("ta").getDouble(0.0);
+    //double ts = table.getEntry("ts").getDouble(0.0);
+    table.getEntry("camMode").setNumber(0);
+
+    //System.out.print("tx: " + tx +"\r");
+    //System.out.print("ty: " + ty +"\r");
+    SmartDashboard.putNumber("tx", tx);
+    SmartDashboard.putNumber("ty", ty);
+    SmartDashboard.putNumber("ta", ta);
+    SmartDashboard.putNumber("Rotation", gyro.getAngle());
+    //SmartDashboard.putNumber("ts", ts);
+
+    double distanceInInches = 53.294 * Math.pow(ta, -0.459) + 3;
+    SmartDashboard.putNumber("distanceInInches", distanceInInches);
+
+    String routeChoice = chooseRoute(tx, distanceInInches);
+
+    SmartDashboard.putString("Route Choice", routeChoice);
+
+    //Drive remember to change this back to 0.75
     if(rightJoy.getY() < -0.15 || rightJoy.getY() > 0.15)
     {
       leadMotorRight.set(0.75 * rightJoy.getY());
@@ -263,8 +479,8 @@ public class Robot extends TimedRobot {
     {
       leadMotorLeft.set(0);
     }
-    
-    //H & V Intake && James is stupid && Square button is intake && X button is out
+    double startPosition = 300;
+    //H & V Intake && James is stupid and gaee && Square button is intake && X button is out
     if(xbox.getAButton()) //intake & hop in
     {
       hIntake.set(-0.6);
@@ -289,27 +505,40 @@ public class Robot extends TimedRobot {
     {
       double startTime = System.currentTimeMillis(); 
       
-      while (System.currentTimeMillis() - startTime < 150)
-      {       
-        hopper.set(-0.3);
+      servo.setAngle(5);
+      while(System.currentTimeMillis() - startTime < 350) {
+        if(System.currentTimeMillis() - startTime < 250 && System.currentTimeMillis() - startTime > 100) {
+          hopper.set(-0.3);
+        }
+        else if(System.currentTimeMillis() - startTime > 250) {
+          hopper.set(0);
+        }
       }
+      servo.setAngle(startPosition);
     }
-    else if (xbox.getRawButton(8))
+    else if (xbox.getRawButtonPressed(8))
     {
       IntakeDOOTDOOT();
+      shooter1.set(0);
+      shooter2.set(0);
     }
     else
     {
       hIntake.set(0.0);
       vIntake.set(0.0);
       hopper.set(0);
+      servo.setAngle(startPosition);
     }
 
-    //shooter
+    //shooter remember to change back to 0.8
     if (xbox.getRawButtonPressed(6)) 
     {
-      shooter1.set(-0.8);
-      shooter2.set(-0.8);
+      //shooter1.set(-1);
+      //shooter2.set(-1);
+      shooter1.setVoltage(-12);
+      shooter2.setVoltage(-12);
+      SmartDashboard.putNumber("shooter1 voltage", shooter1.getBusVoltage());
+      SmartDashboard.putNumber("shooter2 voltage", shooter2.getBusVoltage());
     }
     else if (xbox.getRawButtonPressed(5))
     {
@@ -317,7 +546,7 @@ public class Robot extends TimedRobot {
       shooter2.set(0.0);
     }
     //vision.checkTarget();
-    vision.setCamera(1);
+    //vision.setCamera(1);
   }
 
   /**
